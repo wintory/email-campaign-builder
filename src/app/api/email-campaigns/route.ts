743ed6b000855eb4b771c28c6e33ad7a6dea297d
wintory <thanapol.sakul@gmail.com>
2025-04-20 19:26:02 +0700

    build: integrate route with prisma & add get all campaign

--- src/app/api/email-campaigns/route.ts
+++ src/app/api/email-campaigns/route.ts
@@ -1,5 +1,6 @@
 import { EmailCampaign } from '@/types/emailCampaign'
 import { CampaignSchema } from '@/utilities/schemas/emailCampaign'
+import { PrismaClient } from '@prisma/client'
 import { v4 as uuidV4 } from 'uuid'
 
 export const POST = async (req: Request): Promise<Response> => {
@@ -10,6 +11,7 @@ export const POST = async (req: Request): Promise<Response> => {
     if (!parseResult.success) {
       return Response.json(
         {
+          status: 'error',
           message: 'Validation failed, please check your input',
         },
         { status: 400 }
@@ -17,15 +19,59 @@ export const POST = async (req: Request): Promise<Response> => {
     }
 
     const campaignId = 'campaign-' + uuidV4()
+    const prisma = new PrismaClient()
+    const { campaignName, subjectLine, emailContent, senderEmail } = body
+
+    const newCampaign = await prisma.campaign.create({
+      data: {
+        campaignId,
+        campaignName,
+        subjectLine,
+        emailContent,
+        senderEmail,
+      },
+    })
 
     return Response.json({
+      status: 'success',
       message: 'Email Campaign Created Successfully',
-      data: { campaignId },
+      data: {
+        campaignId: newCampaign?.campaignId,
+        campaignName: newCampaign?.campaignName,
+      },
     })
   } catch (error) {
     console.error('Error creating campaign:', error)
     return Response.json(
       {
+        status: 'error',
+        message: 'Internal Server Error',
+      },
+      { status: 500 }
+    )
+  }
+}
+
+export const GET = async (): Promise<Response> => {
+  try {
+    const prisma = new PrismaClient()
+    const campaigns = await prisma.campaign.findMany()
+
+    return Response.json({
+      status: 'success',
+      message: 'Email Campaigns Retrieved Successfully',
+      data: campaigns.map((campaign) => {
+        return {
+          ...campaign,
+          id: undefined,
+        }
+      }),
+    })
+  } catch (error) {
+    console.error('Error retrieving campaigns:', error)
+    return Response.json(
+      {
+        status: 'error',
         message: 'Internal Server Error',
       },
       { status: 500 }
